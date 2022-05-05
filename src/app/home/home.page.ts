1632c480a28b24f462ba93cbac5f6013e4a0163d
Lucas Renó <lucasreno9@gmail.com>
2022-05-05 19:04:48 -0300

    Primeira aula

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+interface Frase{
+  id: number;
+  frase: string;
+  autor: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,7 +13,15 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  url = 'http://lucasreno.kinghost.net/frase';
+  frase: Frase = {id: 0, frase: '', autor: ''};
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    http.get<any>(this.url).subscribe(
+      resposta => {
+        this.frase = resposta[0];
+      }
+    );
+  }
 
 }
